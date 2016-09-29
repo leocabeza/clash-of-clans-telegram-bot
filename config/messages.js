ae5da83d66eecb20fb41f51d9a5b9a7c82b14a94
Leonardo Cabeza <cabeza.leo@gmail.com>
2016-09-28 20:42:51 -0500

    Added the ability to say hello to new members

---
+++ config/messages.js
@@ -0,0 +1,51 @@
+const messages = {
+  welcomeMsg: 'Hola, #{name}. Bienvenido a `Vzlan Warriors`. \n' + 
+    'Por favor lee primero las reglas escribiendole al bot @VzlanWarriorsBot \n ' + 
+    'el comando /start y luego pídele las reglas con el comando /rules \n ' +
+    ' y escribenos cualquier duda',
+  rules: 'Reglas del clan Vzlan Warriors: \n' +
+    '1.  Es obligatorio hacer sus dos (02) ataques en guerra.  \n' +
+    '2. Mantener el escudo de guerra actualizado. \n' +
+    '3. Las aldeas a atacar en las guerras, se pedirán por el chat del COC y el grupo WS, ' +
+      'en este sentido un jugador caracterizado hará una lista con las peticiones hechas, ' +
+      'la cual será publicada en el Grupo WS.  \n' +
+    '4. No hay regla de espejo en este clan, solo se pide la aldea a la cual ' +
+      'tengas opción de sacar las tres (3) estrellas.  \n' +
+    '5. No botinees JAMÁS a menos que se de esa instrucción por parte ' +
+      'de los jugadores caracterizados del clan.  \n' +
+    '6. Por cada ataque que no hagas en guerra, se aplicará una banca; ' +
+      'Es decir 1 ataque no realizado = 1 banca.  \n' +
+    '7. No tenemos ningún tipo de problema con las donaciones. El que quiera donar dona, ' +
+      'el que no quiera simplemente no lo hace.  \n' +
+    '8. Los castillos de guerra (Ofensivo y Defensivo) se llenan solo con tropas Maxeadas.  \n' +
+    '9. Faltando 8 horas para finalizar la guerra la aldea pedida para el ' +
+      'primer ataque ya cualquiera la puede atacar.  \n' +
+    '10 . Faltando 3 horas para finalizar la guerra la aldea pedida para el ' +
+      'segundo ataque ya cualquiera la puede atacar.  \n' +
+    '11. No expulsamos jugadores sin justificación.  \n' +
+    '12. Respeto entre jugadores y ante todo respeto a las damas del clan (Cero casanovas)  \n' +
+    '13. Solo se dona lo que se pide, si no lo tienes no dones.  \n' +
+    '14. Respetar la lista. No se deben robar aldeas.  \n' +
+    '15. En guerras no se debe atacar sin castillo.  \n' +
+    '16. Procurar pedir tropas de refuerzo del castillo por el grupo WS.  \n' +
+    '17. Guerras contínuas. Se buscaran guerras diariamente entre las 09:00PM y 10:00PM (GMT-4:30).  \n' +
+    '18. No hacer Spam cuando se están pidiendo las aldeas para poder realizar la lista.  \n' +
+    '19. Los ascensos y descensos cada quien se los gana.  \n' +
+    '20. Procurar subir el TH cuando se esté maxeado. Esto no es una carrera es un juego de estrategias.  \n' +
+    '21. Los segundos ataques son para limpiar aldeas y se deben pedir por el chat del COC y grupo WS.  \n' +
+    '22. Para participar en guerras a partir de Th8, se deben poseer todos los héroes y ' +
+      'calderos de hechizos según el nivel de ayuntamiento (Rey, Reina, Centinela, ' +
+      'Caldero Rosa y Caldero Oscuro) siendo de caracter obligatorio que todos esten ' +
+        'disponibles para el día de la guerra.  \n' +
+    '23. En guerras, todos los jugadores de th8 e inferiores, deberán hacer sus dos (02) ' +
+      'ataques durante las primeras 16 horas, así mismo los th9 deberán hacer por lo menos ' +
+      'un (01) ataque en ese mismo lapso de tiempo, de lo contrario se tomará como ataque ' +
+      'no realizado y acarreará la respectiva sanción.  \n' +
+    '24. Se llevará un registro de jugadores que incumplan las precitadas reglas, ' +
+      'específicamente las referentes a guerra (Blacklist); por cada infracción cometida ' +
+      'se aplicará una (01) banca en guerra, las cuales serán acumulativas y consecutivas ' +
+        'hasta seis (06), situación en la cual se estudiará la expulsión del jugador. ' +
+        'Éste registro se reiniciará cada dos (02) temporadas de liga.  \n'
+};
+
+module.exports = messages;
