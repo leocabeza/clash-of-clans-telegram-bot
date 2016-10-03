81a547e69ed7f1b8e21c101ba6e51cb75f754ed1
Leonardo Cabeza <cabeza.leo@gmail.com>
2016-10-03 13:28:58 -0500

    Updated rules

--- config/messages.js
+++ config/messages.js
@@ -6,37 +6,38 @@ const messages = {
   warListHeader: 'War list \u{2b50}: 0-0 | \u{1f52a}: 0-0 \n',
   onlyLeadersCanCreateLists: 'Sólo los lideres mas beios \u{1f60d} pueden crear listas \u{1f60b}',
   rules: 'Reglas del clan Vzlan Warriors: \n' +
-    '1.  Es obligatorio hacer sus dos (02) ataques en guerra.  \n' +
+    '1. Es obligatorio hacer sus dos (02) ataques en guerra.  \n' +
     '2. Mantener el escudo de guerra actualizado. \n' +
-    '3. Las aldeas a atacar en las guerras, se pedirán por el chat del COC y el grupo WS, ' +
+    '3. Las aldeas a atacar en las guerras, se pedirán por el chat del COC y el grupo Telegram, ' +
       'en este sentido un jugador caracterizado hará una lista con las peticiones hechas, ' +
-      'la cual será publicada en el Grupo WS.  \n' +
+      'la cual será publicada en el Grupo Telegram.  \n' +
     '4. No hay regla de espejo en este clan, solo se pide la aldea a la cual tengas opción ' +
-      'de sacar las tres (3) estrellas, siempre y cuando sea de tu mismo nivel de TH. ' +
+      'de sacar las tres (03) estrellas, siempre y cuando sea de tu mismo nivel de TH. ' +
       'En ocasiones se puede subir para hacer scout o bajar para limpiar, pero debe ' +
       'ser consultado con un líder. \n' +
-    '5. No botinees JAMÁS a menos que se de esa instrucción por parte ' +
-      'de los jugadores caracterizados del clan.  \n' +
-    '6. Por cada ataque que no hagas en guerra, se aplicará una banca; ' +
+    '5. No botinees JAMÁS a menos que se dé esa instrucción por parte ' +
+      'del líder o colider(es).  \n' +
+    '6. Por cada ataque que no hagas en guerra, se aplicará una (01) banca; ' +
       'Es decir 1 ataque no realizado = 1 banca.  \n' +
-    '7. No tenemos ningún tipo de problema con las donaciones. El que quiera donar dona, ' +
+    '7. No tenemos ningún tipo de problema con las donaciones. El que quiera donar, dona, ' +
       'el que no quiera simplemente no lo hace.  \n' +
-    '8. Los castillos de guerra (Ofensivo y Defensivo) se llenan solo con tropas Maxeadas.  \n' +
+    '8. Los castillos de guerra (Ofensivo y Defensivo) se llenan solo con tropas al máximo.  \n' +
     '9. Faltando 8 horas para finalizar la guerra la aldea pedida para el ' +
       'primer ataque ya cualquiera la puede atacar.  \n' +
-    '10 . Faltando 3 horas para finalizar la guerra la aldea pedida para el ' +
+    '10. Faltando 3 horas para finalizar la guerra, la aldea pedida para el ' +
       'segundo ataque ya cualquiera la puede atacar.  \n' +
     '11. No expulsamos jugadores sin justificación.  \n' +
     '12. Respeto entre jugadores y ante todo respeto a las damas del clan (Cero casanovas)  \n' +
-    '13. Solo se dona lo que se pide, si no lo tienes no dones.  \n' +
+    '13. Solo se dona lo que se pide, si no lo tienes, no dones.  \n' +
     '14. Respetar la lista. No se deben robar aldeas.  \n' +
-    '15. En guerras no se debe atacar sin castillo.  \n' +
-    '16. Procurar pedir tropas de refuerzo del castillo por el grupo WS.  \n' +
+    '15. En guerras no se debe atacar sin el refuerzo del castillo del clan.  \n' +
+    '16. Procurar pedir tropas de refuerzo del castillo por el grupo Telegram.  \n' +
     '17. Guerras contínuas. Se buscaran guerras diariamente entre las 09:00PM y 10:00PM (GMT-4:30).  \n' +
     '18. No hacer Spam cuando se están pidiendo las aldeas para poder realizar la lista.  \n' +
     '19. Los ascensos y descensos cada quien se los gana.  \n' +
-    '20. Procurar subir el TH cuando se esté maxeado. Esto no es una carrera es un juego de estrategias.  \n' +
-    '21. Los segundos ataques son para limpiar aldeas y se deben pedir por el chat del COC y grupo WS.  \n' +
+    '20. Procurar subir el TH cuando hayas mejorado todo al máximo. Esto no es una carrera ' +
+      'es un juego de estrategias.  \n' +
+    '21. Los segundos ataques son para limpiar aldeas y se deben pedir por el chat del COC y/o grupo Telegram.  \n' +
     '22. Para participar en guerras a partir de TH8, se deben poseer todos los héroes y ' +
       'calderos de hechizos según el nivel de ayuntamiento (Rey, Reina, Centinela, ' +
       'Caldero Rosa y Caldero Oscuro) siendo de caracter obligatorio que todos esten ' +
